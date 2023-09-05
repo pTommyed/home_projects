3efd69e341e8b1161b402a12113486af277700a0
pTommyed <tomaspicha31@gmail.com>
2023-09-05 23:40:21 +0200

    initial_commit

---
+++ geiger_counter/geiger_counter/geiger_counter.ino
@@ -0,0 +1,27 @@
+//####################################################
+//#   geiger_counter
+//#   Copyright (c) 2023 Tomas Picha
+//#   programmed by: Tomas Picha
+//####################################################
+
+
+// DESCRTIPTION: -   sampling frequency ? Hz
+
+
+// HW: ESP8266_WIFI development board 0.91 inch oled, geiger counter kit module
+
+/*----------------------- DEPENDENCES ----------------------------------*/
+
+/*----------------------- DEFINITION -----------------------------------*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
