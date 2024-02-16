771979c4290145348fb1d1a4bacbde647290102d
pTommyed <tomaspicha31@gmail.com>
2024-02-16 14:22:39 +0100

    fuctional test version of outputs

--- inkubator/inkubator.ino
+++ inkubator/inkubator.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+//####################################################
+//#   temperature_logger_bees_version
+//#   Copyright (c) 2024 Tomas Picha
+//#   programmed by: Tomas Picha
+//####################################################
 
+
+// DESCRTIPTION: -   sampling frequency 8 s
+//               -   watchdog used
+
+// HW: Aruino nano, temperatrue sensor DHT22, capacitive moiscure sensor, micros SD card module, ssr relay modul 4ch
+
+/*----------------------- DEPENDENCES ----------------------------------*/
+#include <avr/sleep.h>      // library for sleep
+#include <avr/power.h>      // library for power control
+#include <avr/wdt.h>        // library for default watchdog functions
+#include <avr/interrupt.h>  // library for interrupts handling
+
+#include <SPI.h> // lib to SPI sbernici
+#include <SD.h> // lib to SD modul
+
+/*----------------------- DEFINITION -----------------------------------*/
+
+
+const byte led_indication = 5;
+const byte cs_pin = 10;
+const byte watter_level_mess_pin = A0;
+const byte relay_humidity = 9;
+const byte relay_heater = 8;
+
+bool measure_flag = 1;
+
+static int measure_count = 10;  //counts of analog samples
+int watter_level = -100; 
+
+
+void setup() {
+  serial_initial();
+  analogReference(DEFAULT);
+  pins_initial();
+  bootup_led_indication();
+
+  digitalWrite(relay_heater, HIGH);
+  digitalWrite(relay_humidity, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  voltage_measure();
+  Serial.println(watter_level);
+  delay(2000);
 }
--- inkubator/methods.ino
+++ inkubator/methods.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
-
+/*----------------------serial-initialization------------------------------------*/
+void serial_initial() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/*----------------------pins-initialization------------------------------------*/
+void pins_initial() {
+  pinMode(cs_pin, OUTPUT);
+  pinMode(led_indication, OUTPUT);
+  pinMode(relay_humidity, OUTPUT);
+  pinMode(relay_heater, OUTPUT);
 
+  digitalWrite(relay_humidity, LOW);
+  digitalWrite(relay_heater, LOW);
+  digitalWrite(led_indication, LOW);
+  delay(100);
+}
+
+/*----------------------bootup_led_indication------------------------------------*/
+void bootup_led_indication() {
+  for (int temp = 0;temp < 5; temp++) {
+    digitalWrite(led_indication, HIGH);
+    delay(100);
+    digitalWrite(led_indication, LOW);
+    delay(100);
+  }
+    digitalWrite(led_indication, LOW);
+    delay(100);
+}
+
+/*----------------------voltage_measure------------------------------------*/
+void voltage_measure() {
+  int voltage_temp = 0;
+
+  for (int temp =0;temp < measure_count; temp++) {
+    voltage_temp = voltage_temp + analogRead(watter_level_mess_pin);
+  }
+
+  watter_level = voltage_temp / measure_count;
 }
