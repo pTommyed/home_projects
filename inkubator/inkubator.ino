dade9468068924b44d271d8f2277d676a078718c
pTommyed <tomaspicha31@gmail.com>
2024-02-16 17:23:37 +0100

    non tested full version

--- inkubator/inkubator.ino
+++ inkubator/inkubator.ino
@@ -11,16 +11,27 @@
 // HW: Aruino nano, temperatrue sensor DHT22, capacitive moiscure sensor, micros SD card module, ssr relay modul 4ch
 
 /*----------------------- DEPENDENCES ----------------------------------*/
-#include <avr/sleep.h>      // library for sleep
-#include <avr/power.h>      // library for power control
-#include <avr/wdt.h>        // library for default watchdog functions
-#include <avr/interrupt.h>  // library for interrupts handling
+#include <stdint.h> //due to wdt
+#include <avr/wdt.h> // wdt lib
+
+#include <Wire.h> //This library allows you to communicate with I2C / TWI devices
+#include <LiquidCrystal_I2C.h> // knihovna pro ovladani displaye
 
 #include <SPI.h> // lib to SPI sbernici
 #include <SD.h> // lib to SD modul
 
+#include "DHT.h" // humidity sensore
+
+/*------------------------- Set the adress (0x3F)and pins on the I2C chip used for LCD connections ----------------------*/
+ 
+//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
 /*----------------------- DEFINITION -----------------------------------*/
 
+#define DHTPIN 4     // humidity sensor - pin
+#define DHTTYPE DHT22   // DHT 22 
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor (humidity sensor)
 
 const byte led_indication = 5;
 const byte cs_pin = 10;
@@ -28,24 +39,68 @@ const byte watter_level_mess_pin = A0;
 const byte relay_humidity = 9;
 const byte relay_heater = 8;
 
+const float air_temp_set = 35.0;
+const float hysterezia = 0.5;
+const float air_temp_max = air_temp_set + hysterezia;
+const float air_temp_min = air_temp_set - hysterezia;
+
+const float humidity_max = 90.0;
+const float humidity_min = 60.0;
+
+const int watter_level_max = 213;
+const int watter_level_min = 556;
+const float one_percent = (-1*(watter_level_max - watter_level_min)) / 100.0;
+
+float air_temp = 0.0; // in °C
+float humidity = 1000.0; // in %
+int watter_level = 1000;
+int watter_level_percent = 0; // watter level in percent
+
+bool heater_on_flag = 0;
+bool humidity_on_flag = 0;
+
+bool air_temp_error_flag = 0;
+bool humidity_error_flag = 0;
+
 bool measure_flag = 1;
 
 static int measure_count = 10;  //counts of analog samples
-int watter_level = -100; 
 
+File soubor; // variable for SD card
+String file_name = "DATA.txt";
+
+
+
+/*----------------------setup------------------------------------*/
 
 void setup() {
+  lcd_initial();
   serial_initial();
   analogReference(DEFAULT);
   pins_initial();
+  init_outputs_set();
+  sensors_initial();
+  sd_card_initial();
+  lcd_print_end_of_init();
   bootup_led_indication();
-
-  digitalWrite(relay_heater, HIGH);
-  digitalWrite(relay_humidity, HIGH);
+  wdt_enable(WDTO_4S);  //wdt reset_15MS; 30MS; 60ms; 120MS; 250MS; 500MS; 1S; 2S; 4S; 8S
 }
 
+/*----------------------main_loop------------------------------------*/
+
 void loop() {
-  voltage_measure();
-  Serial.println(watter_level);
-  delay(2000);
+  if (measure_flag == 1) {
+    measure_flag = 0;
+    
+    watter_level_measure();
+    reading_data_DHT22();
+
+    control_heater_output();
+    control_humidity_output();
+
+    write_to_sd();
+    serial_output();
+
+    wdt_reset (); // Reset watchdog counter
+  }
 }
--- inkubator/methods.ino
+++ inkubator/methods.ino
@@ -6,6 +6,34 @@ void serial_initial() {
   }
 }
 
+/*----------------------LCD-initialization------------------------------------*/
+void lcd_initial() {
+  lcd.begin(16,2);         // initialize the lcd for 16 chars 2 lines, turn on backlight
+
+  lcd.setCursor(4,0); 
+  lcd.print("Welcome"); // print text to lcd
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(1,0); 
+  lcd.print("Inicialization"); 
+  delay(250);
+  lcd.setCursor(5,1); 
+  lcd.print("running");
+}
+
+/*----------------------LCD-print_end_of_init------------------------------------*/
+void lcd_print_end_of_init() {
+
+  lcd.clear();
+  lcd.setCursor(1,0); 
+  lcd.print("Inicialization"); 
+  delay(250);
+  lcd.setCursor(6,1); 
+  lcd.print("done");
+  delay(250);
+}
+
 /*----------------------pins-initialization------------------------------------*/
 void pins_initial() {
   pinMode(cs_pin, OUTPUT);
@@ -13,10 +41,19 @@ void pins_initial() {
   pinMode(relay_humidity, OUTPUT);
   pinMode(relay_heater, OUTPUT);
 
+}
+
+/*----------------------init_outputs_set------------------------------------*/
+void init_outputs_set() {
   digitalWrite(relay_humidity, LOW);
   digitalWrite(relay_heater, LOW);
   digitalWrite(led_indication, LOW);
   delay(100);
+}
+
+/*-----------------------Sensors-initialization--------------------------------*/
+void sensors_initial(){
+  dht.begin(); // humidity sensore - DHT22
 }
 
 /*----------------------bootup_led_indication------------------------------------*/
@@ -31,8 +68,57 @@ void bootup_led_indication() {
     delay(100);
 }
 
-/*----------------------voltage_measure------------------------------------*/
-void voltage_measure() {
+/*-------------------------- wake_up_from_Watchdog ----------------*/
+
+ISR( WDT_vect ) {
+  measure_flag = 1;
+}
+
+/*------------------------------ sd_card_initial ------------------------------------*/
+void sd_card_initial() {
+  String message = "";
+
+  while (!SD.begin(cs_pin)) { 
+    Serial.println("SD_card_no_detected!");
+    digitalWrite(led_indication, HIGH);
+    delay(500);
+    digitalWrite(led_indication, LOW);
+    delay(500);
+  }
+
+  if (SD.exists(file_name)) {
+    Serial.println("soubor DATA.txt detected");
+    } else {
+        soubor = SD.open(file_name , FILE_WRITE);
+        soubor.println("-----------------------------");
+        soubor.println("-----------------------------");
+        while (!soubor){
+          Serial.println("error_SD_card!");
+          
+          lcd.clear();
+          lcd.setCursor(1,0); 
+          lcd.print("Inicialization"); 
+          delay(250);
+          lcd.setCursor(1,1); 
+          lcd.print("error_SD_card!");
+          
+          digitalWrite(led_indication, HIGH);
+          delay(500);
+          digitalWrite(led_indication, LOW);
+          delay(500);
+        }
+
+        soubor.println("air_temperatur[°C];  humidity[%];  watter_level[-];  watter_level[%];  heater[-];  humidifier[-];");    
+        soubor.println("-----------------------------");
+        soubor.println("-----------------------------");
+        soubor.close();
+        } 
+
+  delay(250);            
+}
+
+/*----------------------watter_level_measure------------------------------------*/
+void watter_level_measure() {
   int voltage_temp = 0;
 
   for (int temp =0;temp < measure_count; temp++) {
@@ -40,4 +126,151 @@ void voltage_measure() {
   }
 
   watter_level = voltage_temp / measure_count;
+
+  watter_level_convert_percent();
 }
+
+/*----------------------watter_level_percent------------------------------------*/
+void watter_level_convert_percent() {
+  
+  watter_level_percent = (-1*(watter_level_max - watter_level)) / one_percent;
+}
+
+/*-----------------------reading-data-from-DHT22--------------------------------*/
+void reading_data_DHT22(){
+  humidity = dht.readHumidity();
+  air_temp = dht.readTemperature();
+
+  check_reads_data_DHT22();
+  error_detection();
+}
+
+/*-----------------------Check if any reads failed and exit early--------------------------------*/
+void check_reads_data_DHT22(){
+  if (isnan(humidity)) {
+    humidity = 9999;
+    return;
+  }
+  if (isnan(air_temp)) {
+    air_temp = 9999;
+    return;
+  }
+}
+
+/*-------------------------- error_detection  -------------------------*/
+void error_detection() {
+  if (humidity == 9999) {
+    humidity_error_flag = 1;
+  } else {
+    humidity_error_flag = 0;
+  }
+
+  if (air_temp == 9999 ) {
+    air_temp_error_flag = 1;
+  } else {
+    air_temp_error_flag = 0;
+  }
+}
+
+/*------------------------ write_to_SD ---------------------------------------*/
+void write_to_sd() {
+  String message = "";
+  
+  soubor = SD.open(file_name, FILE_WRITE); 
+  while (!soubor){
+    Serial.println("error_SD_card!");
+    digitalWrite(led_indication, HIGH);
+    delay(200);
+    digitalWrite(led_indication, LOW);
+    delay(200);
+    }
+
+  if (air_temp_error_flag == 1) {
+    soubor.print("err;");
+  } else {
+    message = String(air_temp) + ";";
+    soubor.print(message);
+  }
+
+  if (humidity_error_flag == 1) {
+    soubor.print("err;");
+  } else {
+    message = String(humidity) + ";";
+    soubor.print(message);
+  }
+  
+
+  message = String(watter_level) + ";" + String(watter_level_percent) + ";" + String(heater_on_flag) + ";" + String(humidity_on_flag) + ";"; 
+  soubor.println(message);
+  soubor.close();
+  delay(250);
+}
+
+/*------------------------ serial_output ---------------------------------------*/
+void serial_output() {
+  String message = "";
+
+  Serial.println("-----------------------------");
+  
+  if (air_temp_error_flag == 1) {
+    Serial.print("err;");
+  } else {
+    message = String(air_temp) + ";";
+    Serial.print(message);
+  }
+
+  if (humidity_error_flag == 1) {
+    Serial.print("err;");
+  } else {
+    message = String(humidity) + ";";
+    Serial.print(message);
+  }
+  
+
+  message = String(watter_level) + ";" + String(watter_level_percent) + ";" + String(heater_on_flag) + ";" + String(humidity_on_flag) + ";"; 
+  Serial.println(message);
+  Serial.println("-----------------------------"); 
+
+  delay(250);
+}
+
+/*-------------------------- control_heater_output  -------------------------*/
+void control_heater_output() {
+
+  if (air_temp_error_flag == 0) {
+    if ((air_temp < air_temp_min) && (heater_on_flag == 0)) {
+      heater_on_flag = 1;
+      digitalWrite(relay_heater, HIGH);
+      delay(100);
+    } else if ((air_temp >= air_temp_max) && (heater_on_flag == 1)) {
+        heater_on_flag = 0;
+        digitalWrite(relay_heater, LOW);
+        delay(100);
+    }
+  } else if (heater_on_flag == 1) {
+      heater_on_flag = 0;
+      digitalWrite(relay_heater, LOW);
+      delay(100);
+  }
+}
+
+/*-------------------------- control_humidity_output  -------------------------*/
+void control_humidity_output() {
+
+  if (humidity_error_flag == 0) {
+    if ((humidity < humidity_min ) && (watter_level > watter_level_min) && (humidity_on_flag == 0)) {
+      humidity_on_flag = 1;
+      digitalWrite(relay_humidity, HIGH);
+      delay(100);
+    } else if (((air_temp > humidity_max) && (humidity_on_flag == 1)) || (watter_level <= watter_level_min)) {
+        humidity_on_flag = 0;
+        digitalWrite(relay_humidity, LOW);
+        delay(100);
+    }
+  } else if (humidity_on_flag == 1) {
+      heater_on_flag = 0;
+      digitalWrite(relay_humidity, LOW);
+      delay(100);
+  }
+}
+
